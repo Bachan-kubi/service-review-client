dcdd927080acbbb733ff8ca77c3d5a509bbabc4e
Bachan-kubi <felixghagra@gmail.com>
2022-11-09 10:44:21 +0600

    react photoview added in all services component

--- src/Pages/Home/Services/AllServicesCard.js
+++ src/Pages/Home/Services/AllServicesCard.js
@@ -6,32 +6,16 @@ const AllServicesCard = ({ allService }) => {
   const { title, price, description, img } = allService;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      {/* <figure className="px-10 pt-10 w-96">
-        <img
-          src={img}
-          alt="service pic"
-          className="rounded-xl"
-        />
-      </figure> */}
+      
       <PhotoProvider>
         <PhotoView src={img}>
           <img style={{cursor:"pointer"}} src={img} alt="" />
         </PhotoView>
       </PhotoProvider>
-      {/* <figure className="px-10 pt-10 w-96">
-        <img
-          src={img}
-          alt="service pic"
-          className="rounded-xl"
-        />
-      </figure> */}
       <div className="card-body items-center text-center">
         <h2 className="card-title">Service Name: {title}</h2>
         <h2 className="card-title">Price: {price}</h2>
         <p>{description}</p>
-        {/* <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
-        </div> */}
       </div>
     </div>
   );
